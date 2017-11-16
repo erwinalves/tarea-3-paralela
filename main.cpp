e3414f9778a28b7991d0ae64bf7a3feae60dcb92
erwinalves <erwinalbo@hotmail.com>
2017-11-16 09:59:47 -0300

    subiendo carpeta

--- main.cpp
+++ main.cpp
@@ -6,8 +6,20 @@ using namespace std;
 
 int main()
 {
+    file *fichero;
     char cadena[5000],constante=' ',aux[30];
-    char matriz[5000][2];
+    int matriz[5000][2];
+    int i=0;
+    for(int i=0;i<5000;i++)
+        for(int j=0;j<2;j++)
+            matriz[i][j]=0;
+
+    fichero = fopen("puntos.txt","r");
+    if(fichero==NULL){
+        cout<<"no se puede abrir el fichero"<<endl;
+        exit(EXIT_FAILURE);
+    }
+
     ifstream fe("puntos.txt");
     fe.getline(cadena, 5000);
     for(int i=0;i<5000;i++){
